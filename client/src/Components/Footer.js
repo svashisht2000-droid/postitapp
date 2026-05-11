25883a47a733d77875d601f2eda109056ebd80ba
somesh <svashisht2000@gmail.com>
2026-05-11 16:58:09 +0400

    env allowejjj77d

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer" style={{ textAlign: "center", padding: "10px", backgroundColor: "#f1f1f1" }}>
-      <div>Jasmin.Estudillo@utas.edu.om|©2024|PostIT|All Rights Reserved.</div>
+      <div>someshuar.vashisht@utas.edu.om|©2024|PostIT|All Rights Reserved.</div>
     </footer>
   );
 };
